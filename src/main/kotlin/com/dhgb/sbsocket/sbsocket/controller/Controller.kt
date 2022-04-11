95f5ed65a2e383753d67457862a2d3bac53f0c4a
diegoguerreroborda <diego.guerrero02@uptc.edu.co>
2022-04-11 15:38:35 -0500

    fix: received and send message through sockets

--- src/main/kotlin/com/dhgb/sbsocket/sbsocket/controller/Controller.kt
+++ src/main/kotlin/com/dhgb/sbsocket/sbsocket/controller/Controller.kt
@@ -9,10 +9,20 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class Controller{
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
+    @MessageMapping("/current")
+    @SendTo("/client/messages")
     fun receivedMessage(@Payload message: String): String {
         println("Llegó $message")
         return message
     }
+
+//    @Scheduled(fixedRate = 5000)
+//    fun work1() {
+//        println("Va a enviar")
+//        @SendTo("/topic/messages")
+//        fun receivedMessage(): String {
+//            println("Va a enviar cada 5 segundos")
+//            return "Va a enviar cada 5 segundos"
+//        }
+//    }
 }
--- src/main/kotlin/com/dhgb/sbsocket/sbsocket/socket/WebSocketConfig.kt
+++ src/main/kotlin/com/dhgb/sbsocket/sbsocket/socket/WebSocketConfig.kt
@@ -1,32 +1,41 @@
 package com.dhgb.sbsocket.sbsocket.socket
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.converter.MessageConverter
+import org.springframework.messaging.converter.StringMessageConverter
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.config.annotation.*
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.server.RequestUpgradeStrategy
 import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler
+
 
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketConfig: WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        super.registerStompEndpoints(registry)
         println("registry")
         val upgradeStrategy: RequestUpgradeStrategy = TomcatRequestUpgradeStrategy()
         registry.addEndpoint("/ws").withSockJS()
-        registry.addEndpoint("/chat").withSockJS()
-        registry.addEndpoint("/chat")
+        registry.addEndpoint("/current").withSockJS()
+        registry.addEndpoint("/current")
                 .setHandshakeHandler(DefaultHandshakeHandler(upgradeStrategy))
                 .setAllowedOrigins("*")
 
     }
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
-//        super.configureMessageBroker(registry)
         println("config")
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/client")
+        config.setApplicationDestinationPrefixes("/server")
+    }
+
+    override fun configureMessageConverters(messageConverters: MutableList<MessageConverter>): Boolean {
+        val strConvertor = StringMessageConverter()
+        messageConverters.add(strConvertor)
+        return true
     }
 }
